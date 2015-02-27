70f9bb07f1a2deb85bbb2c52b9834d4d9658fd69
def <dennis@felsin9.de>
2015-02-27 04:39:33 +0100

    Add asynchttpserver_parallel (not working well yet)

---
+++ asynchttpserver_parallel.nim
@@ -0,0 +1,17 @@
+import asynchttpserver, asyncdispatch, os, strutils
+
+proc cb(req: Request) {.async.} =
+  await req.respond(Http200, "Hello, World!")
+
+proc run(server: AsyncHttpServer) =
+  register(TAsyncFD(server.socket.fd))
+  asyncCheck server.serveParallel(cb)
+  runForever()
+
+var server = newAsyncHttpServer()
+server.initialize(Port(8080))
+
+var threads = newSeq[TThread[AsyncHttpServer]](parseInt(paramStr(1)))
+for t in threads.mitems:
+  t.createThread run, server
+joinThreads threads
