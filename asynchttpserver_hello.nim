f8cc61f21cea59db33b9fa7e1c3f33c3766bfc80
def <dennis@felsin9.de>
2015-02-27 01:39:45 +0100

    Simplify asynchttpserver_hello

--- asynchttpserver_hello.nim
+++ asynchttpserver_hello.nim
@@ -1,11 +1,7 @@
 import asynchttpserver, asyncdispatch
-var server = newAsyncHttpServer()
+
 proc cb(req: Request) {.async.} =
-  #for i in 1..10:
-    #var fut = newFuture[string]("asyncnet.acceptAddr")
-    #fut.complete("foo")
-    #echo repr(fut)
   await req.respond(Http200, "Hello, World!")
 
-asyncCheck server.serve(Port(8080), cb)
+asyncCheck newAsyncHttpServer().serve(Port(8080), cb)
 runForever()
